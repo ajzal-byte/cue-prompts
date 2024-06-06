8fa818e85d748af6155a351bed7b661f78ba0fa0
ajzal-byte <ajzalaju10@gmail.com>
2024-06-06 22:50:27 +0530

    added a favicon, somehow

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,11 +5,18 @@ import { Nav, Provider } from "@components";
 export const metadata = {
   title: "cue",
   description: "Explore and share elegant AI prompts",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
 };
 const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
+      {/* <Head>
+      <link rel="shortcut icon" href="/logo.webp" />
+    </Head> */}
       <body>
+      <link rel="icon" href="/images/favicon.ico" sizes="any" />
         <Provider>
           <div className="main">
             <div className="gradient" />
