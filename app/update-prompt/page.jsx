31a39943857a1b10114f178611aa9c1045bdb9e3
ajzal-byte <ajzalaju10@gmail.com>
2024-06-08 12:03:59 +0530

    suspense removed

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Form } from "@components";
 
@@ -65,12 +65,4 @@ const UpdatePrompt = () => {
   );
 };
 
-const EditPrompt = () => {
-  return (
-    <Suspense>
-      <UpdatePrompt />
-    </Suspense>
-  );
-};
-
-export default EditPrompt;
+export default UpdatePrompt;
